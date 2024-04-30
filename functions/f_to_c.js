fa6ee546886c4b7d0a067b9d665d25416d2f19d9
Julian Pompey <jupompey00@gmail.com>
2024-04-30 19:00:37 -0400

    checkpoint

---
+++ functions/f_to_c.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function convertFtoC(currentTemp){
+return (currentTemp - 32) / 1.8;
+}
+
+console.log(convertFtoC(212));
+console.log(convertFtoC(90));
+console.log(convertFtoC(72));
+console.log(convertFtoC(32));
+console.log(convertFtoC(0));
+console.log(convertFtoC(-40));
---
+++ functions/tax_functions.js
@@ -0,0 +1,14 @@
+"use strict";
+//code here
+
+function getSocSecurityTax(){
+
+}
+
+function getMedicareTax(){
+
+}
+
+function getFederalTax(){
+
+}
