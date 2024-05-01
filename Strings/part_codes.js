bbc9c91182a218a0184bbe8771060e7d50804464
Julian Pompey <jupompey00@gmail.com>
2024-05-01 14:34:30 -0400

    bugfix

--- Strings/part_codes.js
+++ Strings/part_codes.js
@@ -8,44 +8,44 @@ let productCode2 = "ACE:1-12";
 
 //returns all characters before the :
 function getSupplier(code){
-return code.substring(0,code.indexOf(":"));
-}
+    return code.substring(0,code.indexOf(":"));
+    }
 
 //returns all characters between : and -
-function getProductNumber(code){
-return code.substring(code.indexOf(":")+1, code.indexOf("-"));
+function getProductNumber(code) {
+    return code.substring(code.indexOf(":") + 1, code.indexOf("-"));
 }
 
 //returns all characters after the -
-function getSize(code){
-return code.substring(code.indexOf("-")+1)
+function getSize(code) {
+    return code.substring(code.indexOf("-") + 1)
 }
 
 
 //outputs the parts info in a concise way based on the earlier functions
-function outputPartInfo(code){ 
-let supplier = getSupplier(code);
-let productNumber = getProductNumber(code);
-let size = getSize(code);
-let sizeExpanded;
+function outputPartInfo(code) {
+    let supplier = getSupplier(code);
+    let productNumber = getProductNumber(code);
+    let size = getSize(code);
+    let sizeExpanded;
 
-//If-Else block that checks if the size is in SML format or is a number.
-if(isNaN(Number(size))){
-    if(size.toUpperCase() == "L"){
-        sizeExpanded = "large (L)";
+    //If-Else block that checks if the size is in SML format or is a number.
+    if (isNaN(Number(size))) {
+        if (size.toUpperCase() == "L") {
+            sizeExpanded = "large (L)";
+        }
+        if (size.toUpperCase() == "M") {
+            sizeExpanded = "medium (M)";
+        }
+        if (size.toUpperCase() == "S") {
+            sizeExpanded = "small (S)";
+        }
+    } else {
+        sizeExpanded = `size ${size}`;
     }
-    if(size.toUpperCase() == "M"){
-        sizeExpanded = "medium (M)";        
-    }
-    if(size.toUpperCase() == "S"){
-        sizeExpanded = "small (S)";
-    }
-} else {
-    sizeExpanded = `size ${size}`;
+
+    return `The ${sizeExpanded} part ${productNumber} is supplied by ${supplier}`;
 }
 
-return `The ${sizeExpanded} part ${productNumber} is supplied by ${supplier}`;
-}
 
-
-console.log(outputPartInfo([productCode0]));
+console.log(outputPartInfo(productCode0));
